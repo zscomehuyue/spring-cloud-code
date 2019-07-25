9bd43fb5bd2831bf701db7e29e262ae8e781c6ca
zscome <zscomehuyue@126.com>
2019-07-25 23:01:43 +0800

    ad

--- ch2-1/ch2-1-eureka-client/src/main/java/cn/springcloud/book/Ch21EurekaClientApplication.java
+++ ch2-1/ch2-1-eureka-client/src/main/java/cn/springcloud/book/Ch21EurekaClientApplication.java
@@ -4,8 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ *
+ * FIXME 1，@EnableDiscoveryClient注解是基于spring-cloud-commons依赖，并且在classpath中实现；
+ *
+ * FIXME 2，@EnableEurekaClient注解是基于spring-cloud-netflix依赖，只能为eureka作用；
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
+//@EnableEurekaClient
 public class Ch21EurekaClientApplication {
 
 	public static void main(String[] args) {
